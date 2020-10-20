195db447680bdc1b2ca83e718b6863a77d22109a
Jonathan Ramanantsoa <jonathan.rama2005@gmail.com>
2020-10-20 17:26:53 +0800

    Fixed bug

--- website/script.js
+++ website/script.js
@@ -1,26 +1,25 @@
 const apiLink = 'https://shorty2587.herokuapp.com/create/'
 
-const options = {
-    method: 'POST',
-    body: JSON.stringify({ url: 'https://reddit.com' })
-}
-
 async function create_url() {
     console.log('function called')
     let url = document.getElementById('link-input').value
-    let response = await fetch(apiLink, options)
+    let response = await fetch(apiLink, {
+        method: 'POST',
+        body: JSON.stringify({ 'url': url }),
+        headers: { 'content-type': 'application/json' }
+    })
     console.log(response)
-    // let short = JSON.parse(response.body['url'])
-    // console.log(short)
-    // const urlHolder = document.getElementById('url-place')
-    // const errorHolder = document.getElementById('error-place')
-    // if (short) {
-    //     urlHolder.innerHTML = short
-    //     errorHolder.innerHTML = ''
-    // } else {
-    //     urlHolder.innerHTML = ''
-    //     errorHolder.innerHTML = 'Invalid url'
-    // }
-    // console.log('function ended')
+    let short = JSON.parse(response.text())
+    console.log(short)
+    const urlHolder = document.getElementById('url-place')
+    const errorHolder = document.getElementById('error-place')
+    if (short) {
+        urlHolder.innerHTML = short
+        errorHolder.innerHTML = ''
+    } else {
+        urlHolder.innerHTML = ''
+        errorHolder.innerHTML = 'Invalid url'
+    }
+    console.log('function ended')
 
 }
