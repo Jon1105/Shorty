30a865ffc7eac1e782e2cb346cb3ef04b155d578
Jonathan Ramanantsoa <jonathan.rama2005@gmail.com>
2020-10-20 08:56:18 +0800

    Fixed bug

--- website/script.js
+++ website/script.js
@@ -1,23 +1,23 @@
 const apiLink = 'https://shorty2587.herokuapp.com/create/'
 
-function create_url() {
+async function create_url() {
     console.log('function called')
     let url = document.getElementById('link-input').value
-    fetch(apiLink, {
+    let response = await fetch(apiLink, {
         method: "POST",
         body: JSON.stringify({ 'url': url })
-    }).then((response) => {
-        let short = JSON.parse(response.body)['url']
-        console.log(response)
-        const urlHolder = document.getElementById('url-place')
-        const errorHolder = document.getElementById('error-place')
-        if (short) {
-            urlHolder.innerHTML = short
-            errorHolder.innerHTML = ''
-        } else {
-            urlHolder.innerHTML = ''
-            errorHolder.innerHTML = 'Invalid url'
-        }
-        console.log('function ended')
     })
+    let short = JSON.parse(response.body['url'])
+    console.log(short)
+    const urlHolder = document.getElementById('url-place')
+    const errorHolder = document.getElementById('error-place')
+    if (short) {
+        urlHolder.innerHTML = short
+        errorHolder.innerHTML = ''
+    } else {
+        urlHolder.innerHTML = ''
+        errorHolder.innerHTML = 'Invalid url'
+    }
+    console.log('function ended')
+
 }
